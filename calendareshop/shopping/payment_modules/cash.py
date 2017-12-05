a6a26c6af55d6ad19b4a2887d9ec770531df75f8
Vojtěch Oram <vojtech@oram.cz>
2017-12-05 18:47:48 +0100

    shopping: DOnt mark cash payment as paid after creating new order

--- calendareshop/shopping/payment_modules/cash.py
+++ calendareshop/shopping/payment_modules/cash.py
@@ -10,12 +10,35 @@ import logging
 
 from django.utils.translation import ugettext_lazy as _
 
-from plata.payment.modules.cod import PaymentProcessor as CODPaymentProcessor
+import plata
+from plata.payment.modules.base import ProcessorBase
+from plata.shop.models import OrderPayment
 
 
 logger = logging.getLogger(__name__)
 
 
-class PaymentProcessor(CODPaymentProcessor):
+class PaymentProcessor(ProcessorBase):
     key = 'cash'
     default_name = _('Cash')
+
+    # basically same as BANK
+    def process_order_confirmed(self, request, order):
+        if not order.balance_remaining:
+            return self.already_paid(order)
+
+        logger.info('Processing order %s using CASH' % order)
+
+        payment = self.create_pending_payment(order)
+
+        payment.status = OrderPayment.PROCESSED
+        payment.save()
+        order = order.reload()
+
+        if plata.settings.PLATA_STOCK_TRACKING:
+            StockTransaction = plata.stock_model()
+            self.create_transactions(
+                order, _('sale'),
+                type=StockTransaction.SALE, negative=True, payment=payment)
+
+        return self.shop.redirect('plata_order_success')

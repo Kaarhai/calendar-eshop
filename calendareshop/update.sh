f909a6e8f65249b76477ee1737d41216a443dbc8
Vojtech Oram <vojtech@oram.cz>
2018-05-08 10:49:45 +0200

    cron: Added cron file and updated reinstall script

--- calendareshop/update.sh
+++ calendareshop/update.sh
@@ -2,6 +2,8 @@
 #
 # Script for deploying new changes in production.
 
+PROJECT_PATH=/www/calendar-eshop/calendar-eshop/calendareshop
+
 set -ex
 
 # save credentials for later use
@@ -19,5 +21,10 @@ pip install -r ../requirements.txt
 # collect static and compress
 ./manage.py collectstatic --noinput
 #./manage.py compress
+
+# update cron
+cp $PROJECT_PATH/calendareshop.cron /etc/cron.d/calendareshop.cron
+crontab /etc/cron.d/calendareshop.cron
+/etc/init.d/cron reload 
 
 touch confs/production/uwsgi.ini

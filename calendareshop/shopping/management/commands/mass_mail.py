4ba4141034fe0042f0b3eec6194efd4b5c815f66
Vojtech Oram <vojtech@oram.cz>
2019-10-16 11:03:15 +0200

    mass_mail update

--- calendareshop/shopping/management/commands/mass_mail.py
+++ calendareshop/shopping/management/commands/mass_mail.py
@@ -32,19 +32,19 @@ class Command(BaseCommand):
         doit = options['doit']
         send_test = options['send_test']
 
-        year = datetime.datetime.today().year
+        year = datetime.datetime.today().year + 1
 
         blacklist = get_emails_from_file('blacklist.txt')
 
         sent_emails = set()
-        #sent_emails = get_emails_from_file('member_mails.txt')
+        sent_emails = get_emails_from_file('member_mails.txt')
 
-        emails = get_emails_from_file('emails_customers_2018.txt')
+        #emails = get_emails_from_file('emails_customers_2018.txt')
 
         #emails = sent_emails
-        #emails = set()
-        #emails.update(list(CustomOrder.objects.filter(personal_information_consent=True, language_code='cs').values_list('email', flat=True)))
-        #emails.update(list(NewsletterSubscription.objects.values_list('email', flat=True)))
+        emails = set()
+        emails.update(list(CustomOrder.objects.filter(personal_information_consent=True, language_code='cs').values_list('email', flat=True)))
+        emails.update(list(NewsletterSubscription.objects.values_list('email', flat=True)))
         emails -= sent_emails
         emails -= blacklist
         count = len(emails)
@@ -55,9 +55,10 @@ class Command(BaseCommand):
                 'flaiming@gmail.com',
                 #'kitty@draci.info',
                 #'exander77@gmail.com',
+                #'kaarhai@gmail.com',
             ]
-        else:
-            logger.info('Total of emails that will be send: %s', count)
+            count = len(emails)
+        logger.info('Total of emails that will be send: %s', count)
 
         email_pre_preorder = (
             u"Připravujeme předobjednávky kalendáře Draci.info %s" % year,
@@ -72,53 +73,46 @@ Přejeme Vám pěkné léto a těšíme se na Vás na podzim s novým kalendář
 Organizační tým projektů Draci.info""")
 
         email_preorder = (
-            u"Předobjednávka kalendáře Draci.info %s otevřena" % year,
+            u"Předobjednávky kalendáře Draci.info %s jsou otevřeny!" % year,
             u"""Pěkný den,
 
-zahájili jsme <a href="https://kalendar.draci.info/">předobjednávky kalendáře Draci.info {year}</a>!
-I letos se můžete těšit na 13 dračích obrázků od autorů z <a href="https://draci.info" target="_blank">Draci.info</a>.
-Hotové kalendáře budeme rozesílat koncem listopadu.
+<a href="https://kalendar.draci.info/">kalendář Draci.info 2020</a> je nyní v předprodeji!
+I letos se můžete těšit na 13 dračích obrázků od českých a slovenských autorů z Draci.info.
+Hotové kalendáře budeme rozesílat do nových domovů na konci října - po předprodeji bude k dispozici pouze omezené množství kalendářů, proto doporučujeme předobjednat kalendář co nejdříve.
 
-Letos poprvé připravujeme i stolní kalendář, který bude možné předobjednat asi za týden. Budeme Vás o tom informovat emailem. Prosíme o trpělivost :)
+Děkujeme za Vaši přízeň a přejeme krásný, barevný podzim!
 
 Organizační tým projektů Draci.info""".format(year=year))
 
         email_members = (
-            u"Předobjednávky kalendářů %s otevřeny" % year,
+            u"Předobjednávka kalendáře %s pro členy draci.info otevřena!" % year,
             u"""Ahoj!
 
-Jako členovi Draci.info Ti nabízíme kalendáře Draci.info, pexeso a omalovánky a speciální cenu.
+Jako členovi Draci.info Ti nabízíme kalendář Draci.info a pexeso za speciální cenu.
 
 <b>Jak objednat?</b>
 1) Sečti ceny produktů, které si chceš objednat a připočti poštovné. (ceny viz níže)
 2) Pošli platbu na <b>infácký účet 2901050776 / 2010</b> do zpráy pro příjemce napiš svou přezdívku na infu a obsah objednávky
-3) Odpověz na tento email NEBO napiš Kitty vzkaz obsahující: info za co jsi zaplatil(a) + reálné jméno a adresa, nebo info o vyzvednutí na sletu
+3) Odpověz na tento email NEBO napiš Kay vzkaz obsahující: info za co jsi zaplatil(a) + reálné jméno a adresa, nebo info o vyzvednutí na sletu
 
 <b>Ceny</b>
 Nástěnný kalendář {year}: 140 Kč/ks
-Stolní kalendář {year}: 180 Kč/ks *
-Pexeso: 15 Kč **
-Omalovánky: 170 Kč ***
+Pexeso: 15 Kč *
 
-<b>Poštové</b>
+<b>Poštovné</b>
 Poštovné za 1 kalendář po ČR = 80 Kč
 Poštovné za 2-4 kalendáře po ČR = 90 Kč
 Poštovné za 1 kalendář do SK = 190 Kč
 Poštovné za 2 kalendáře do SK = 270 Kč
 Poštovné za 3-4 kalendáře do SK = 410 Kč
-* Ke každému ks nástěnného kalendáře můžeš objednat 1ks stolního bez navýšení poštovného -> za každý ks stolního nad to navíc si připočti 40 Kč. Poštovné za jeden samostatný stolní kalendář je 60 Kč.
-** Pexeso poštou lze pouze s kalednářem.
-*** 1 ks omalovánek nenavyšuje cenu poštovného
+* Pexeso poštou lze pouze s kalendářem, poštovné se počítá pouze za kalendář.
 
-Příklad: Objednávám 1 nástěnný, 2 stolní, 1 omalovánku a 1 pexeso. Za poštovné zaplatím 80 + 40 = 120 Kč. Za produkty 140 + 2x180 + 170 + 15 = 685 Kč. Celkem pošlu na účet 805 Kč.
-(Pokud si nebudeš vědět rady s výpočtem poštovného, napiš Kitty)
+(Pokud si nebudeš vědět rady s výpočtem poštovného, napiš Kay)
 
-<b>Kalendáře budou rozesílány/rozdávány v listopadu/prosinci.</b>
+<b>Kalendáře budou rozesílány/rozdávány na konci října.</b>
 
 <b>Autoři</b>
 Každý autor má nárok na 1 kus kalendáře zdarma a to včetně poštovného na celou objednávku.
-Pro stolní platí to samé, pokud v něm má autor alespoň 2 obrázky.
-Autor si může vybrat, jestli chce stolní nebo nástěnný kalendář.
 
 Těšíme se na tvou objednávku!
 
@@ -147,7 +141,7 @@ Organizační tým projektů Draci.info""".format(year=year))
 
 S radostí Vám oznamujeme, že tento rok poprvé nabízíme veřejnosti i <b>stolní dračí kalendář</b>!
 Předobjednat si ho můžete v našem <a href="https://kalendar.draci.info/">eshopu</a> za 250 Kč.
-Tisk i rozesílání proběhne společně s nástěnnými kalednáři koncem listopadu.
+Tisk i rozesílání proběhne společně s nástěnnými kalendáři koncem listopadu.
 
 Pokud máte zájem o stolní i nástěnný kalendář, připravili jsme pro Vás <b>speciální balíček za výhodnou cenu</b>!
 Vše najdete na eshopu.
@@ -219,12 +213,12 @@ Přejeme Vám veselý Dračí rok 2019!
 
 Organizační tým projektů Draci.info""".format(year=year))
 
-        current_email = email_feedback
-        subject = current_email[0]
+        current_email = email_preorder
+        subject = current_email[0] + (' TEST' if send_test else '')
         text = current_email[1]
 
+        logger.info(u'Sending email:\nSubject: %s\nText:\n%s', subject, text)
         for email in emails:
-            logger.info(u'Sending email for %s:\n%s', email, text)
             if doit:
                 mail.send(
                     [email],
